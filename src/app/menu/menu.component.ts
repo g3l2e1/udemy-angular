b45d8639350766eb88b8ab02c83cb7f636c4d7e7
Gleyson Barroso <g3l2e1@gmail.com>
2019-01-29 21:36:54 -0300

    Primeiro botão

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  public texto:string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.texto = "Olá mundo";
+  }
+
+  clicou(){
+    this.texto = "Clicou?";
+  }
+
+
+}
