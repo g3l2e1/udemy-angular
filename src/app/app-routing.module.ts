234b023e26d627ad422d726f6f7c159ba8d36d08
Gleyson Barroso <g3l2e1@gmail.com>
2019-01-30 20:57:01 -0300

    Criando módulos independentes para home e usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { UsuarioComponent } from './usuario/usuario.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'usuario', component: UsuarioComponent },
+  { path: 'home', loadChildren: './home/home.module#HomeModule' },
+  { path: 'usuario', loadChildren: './usuario/usuario.module#UsuarioModule' },
   { path: '', pathMatch: 'full', redirectTo:'/home' },
 ];
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {useHash:true})
   ],
   exports: [ RouterModule ]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
-import { UsuarioComponent } from './usuario/usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
-    FooterComponent,
-    HomeComponent,
-    UsuarioComponent
+    MenuComponent
   ],
   imports: [
     BrowserModule,
